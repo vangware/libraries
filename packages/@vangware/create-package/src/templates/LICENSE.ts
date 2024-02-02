6e2dbdf4754ece15970c07615368f037540510f2
Lou Cyx <git@lou.cx>
2024-02-02 16:09:31 -0600

    📄 update licenses.

--- packages/@vangware/create-package/src/templates/LICENSE.ts
+++ packages/@vangware/create-package/src/templates/LICENSE.ts
@@ -1,6 +1,6 @@
 export default () => `MIT License
 
-Copyright (c) ${new Date().getFullYear()}-present Vangware
+Copyright (c) ${new Date().getFullYear()}-present Lou Cyx
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of
 this software and associated documentation files (the "Software"), to deal in
